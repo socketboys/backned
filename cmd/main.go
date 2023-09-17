facfd1ab2d63842a728fba66cbb3265bb21135a6
rajatkr <rajat.kumar@venturasecurities.com>
2023-09-17 12:15:08 +0530

    [feat] logger and swagger left

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,18 @@ import (
 	"project-x/internal/utils"
 )
 
+// @title SocketBoys/Backned APIs
+// @version 1.0
+// @description Testing Swagger APIs.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8081
+// @schemes http
+
 func main() {
 	gin.SetMode(gin.TestMode)
 
